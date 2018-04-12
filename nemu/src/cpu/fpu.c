597a0e8db466ab53462569fd8002628d892c5e19
Yudong Zhang <141120159@smail.nju.edu.cn>
2018-04-12 02:33:24 -0700

    Fixed bugs in exp_res

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -247,7 +247,7 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a) {
 	uint32_t exp_res = 0;
 
 	/* TODO: exp_res = ? leave space for GRS bits. */
-	exp_res = fa.exponent + fb.exponent;
+	exp_res = fa.exponent + fb.exponent - 127 - 20;
 	
 	return internal_normalize(f.sign, exp_res, sig_res);
 }
