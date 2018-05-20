262070ce776600cd8d2af36f628941729f43b8cb
tracer-pa2018 <tracer@njuics.org>
2018-05-20 16:39:42 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  16:39:42 up 2 days, 10:32,  8 users,  load average: 0.15, 0.07, 0.01 cf73553c11e3a0f541aaacbdab67348a5489a84

--- nemu/src/cpu/cpu.c
+++ nemu/src/cpu/cpu.c
@@ -113,6 +113,7 @@ int exec_inst() {
 	uint8_t opcode = 0;
 	// get the opcode
 	opcode = instr_fetch(cpu.eip, 1);
+	printf("opcode = 0x%x\n");
 	// instruction decode and execution
 	int len = opcode_entry[opcode](cpu.eip, opcode);
 	return len;
