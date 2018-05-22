396d461d1f3350754f34eaff9b149bdb8644ed76
Yudong Zhang <141120159@smail.nju.edu.cn>
2018-05-22 13:05:57 -0700

    Add all cmp to opcode map

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -7,10 +7,15 @@ static void instr_execute_2op() {
 	alu_sub(opr_src.val, opr_dest.val);
 }
 
+
+make_instr_impl_2op(cmp, i, rm, b)
 make_instr_impl_2op(cmp, i, rm, v)
-
-
-
+make_instr_impl_2op(cmp, i, a, b)
+make_instr_impl_2op(cmp, i, a, v)
+make_instr_impl_2op(cmp, r, rm, b)
+make_instr_impl_2op(cmp, r, rm, v)
+make_instr_impl_2op(cmp, rm, r, b)
+make_instr_impl_2op(cmp, rm, r, v)
 
 make_instr_func(cmp_si2rm_bv) {
 	int len = 1;
