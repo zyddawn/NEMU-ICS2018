bec76d0cfb7113810cfa7bbbaf45374240bfa85e
tracer-pa2018 <tracer@njuics.org>
2018-05-22 16:08:44 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  16:08:44 up 3 days,  4:22, 10 users,  load average: 0.08, 0.05, 0.00 c3b2a507e1d83664f1ac1cb99419e149ffecb7f

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -1,8 +1,18 @@
 #include "cpu/instr.h"
+#include "cpu/reg.h"
+
+#define DEBUG
 
 make_instr_func(ret_near) {
 	OPERAND pop_eip;
 	pop_eip.data_size = data_size;
+	
+#ifdef DEBUG
+	bool success=false;
+	int esp = get_reg_val("%esp", success);
+	if (success)
+		printf("before ret %esp = 0x%x\n", esp);
+#endif
 	if (data_size == 16) {
 		pop_eip.addr = REG_SP;
 		operand_read(&pop_eip);
@@ -15,6 +25,9 @@ make_instr_func(ret_near) {
 		cpu.esp += 4;
 		cpu.eip = pop_eip.val;
 	}
+	esp = get_reg_val("%esp", success);
+	if (success)
+		printf("after ret %esp = 0x%x\n", esp);
 	return 0;
 }
 
