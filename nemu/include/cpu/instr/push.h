e10e583f0f045ba2244ba645b27df552240ac537
tracer-pa2018 <tracer@njuics.org>
2018-05-22 14:13:18 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  14:13:18 up 3 days,  2:26, 10 users,  load average: 0.00, 0.00, 0.00 3fca452162e64eb5ea73d0261517f21b48b79ea8

--- nemu/include/cpu/instr/push.h
+++ nemu/include/cpu/instr/push.h
@@ -1,14 +1,14 @@
 #ifndef __INSTR_PUSH_H__
 #define __INSTR_PUSH_H__
 
-make_instr_func(push, eax, v)
-make_instr_func(push, ecx, v)
-make_instr_func(push, edx, v)
-make_instr_func(push, ebx, v)
-make_instr_func(push, esp, v)
-make_instr_func(push, ebp, v)
-make_instr_func(push, esi, v)
-make_instr_func(push, edi, v)
+make_instr_func(push_eax_v)
+make_instr_func(push_ecx_v)
+make_instr_func(push_edx_v)
+make_instr_func(push_ebx_v)
+make_instr_func(push_esp_v)
+make_instr_func(push_ebp_v)
+make_instr_func(push_esi_v)
+make_instr_func(push_edi_v)
 
 #endif
 
