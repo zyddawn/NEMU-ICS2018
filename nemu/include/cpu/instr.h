40dcf376fe8de8105df5d9029737b2d16b389033
tracer-pa2018 <tracer@njuics.org>
2018-05-21 18:07:12 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  18:07:12 up 2 days, 19:32,  9 users,  load average: 0.00, 0.00, 0.00 378627f7f9eba55eea89b884da9cf088a36b48d1

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -22,6 +22,6 @@ extern uint8_t data_size;
 #include "cpu/instr/x87.h"
 
 /* TODO: add more instructions here */
-
+#include "cpu/instr/jcc.h"
 
 #endif
---
+++ nemu/include/cpu/instr/jcc.h
@@ -0,0 +1,12 @@
+#ifndef __INSTR_JCC_H__
+#define __INSTR_JCC_H__
+
+make_instr_func(jnp_near);
+
+#endif
+
+
+
+
+
+
