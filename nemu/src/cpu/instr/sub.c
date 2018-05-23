b9b00a5e16d8fcf9dcc00ba12a8c77bd8da75157
tracer-pa2018 <tracer@njuics.org>
2018-05-23 00:33:06 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  00:33:06 up 3 days,  9:33, 12 users,  load average: 0.00, 0.00, 0.00 1a307345a1eb69c26e89c5307d13e5b13f7ed7b

---
+++ nemu/src/cpu/instr/sub.c
@@ -0,0 +1,40 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op() {
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val = alu_sub(opr_src.val, opr_dest.val);
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(sub, i, a, b)
+make_instr_impl_2op(sub, i, a, v)
+make_instr_impl_2op(sub, i, rm, b)
+make_instr_impl_2op(sub, i, rm, v)
+make_instr_impl_2op(sub, r, rm, b)
+make_instr_impl_2op(sub, r, rm, v)
+make_instr_impl_2op(sub, rm, r, b)
+make_instr_impl_2op(sub, rm, r, v)
+
+make_instr_func(sub_si2rm_bv) {
+	int len = 1;
+	OPERAND opr_src, opr_dest;
+	decode_data_size_bv
+	decode_operand_i2rm
+	
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val = alu_sub(sign_ext(opr_src.val, 8), opr_dest.val);
+	operand_write(&opr_dest);	
+	return len;
+}
+
+
+
+
+
+
+
+
+
+
