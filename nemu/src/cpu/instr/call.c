16876d3bdad61820427a3b900c3003e7da5fbcd5
tracer-pa2018 <tracer@njuics.org>
2018-06-01 00:13:42 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  00:13:42 up 4 days,  4:12, 12 users,  load average: 0.14, 0.07, 0.03 c2a9581253fae68e4fe8d00e9f6691f7f13594c0

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -26,7 +26,7 @@ make_instr_func(call_near_r_v) {
 		operand_write(&push_eip);
 		cpu.eip = eip + disp.val;
 	}
-#ifdef DEBUG
+#ifdef xx
 	while(1) { 	
 		printf("data size = %d, push eip = 0x%x\n", data_size, push_eip.val);
 	}
