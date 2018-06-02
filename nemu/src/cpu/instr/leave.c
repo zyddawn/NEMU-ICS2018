8367477b988ff40a0f2ac404ff83672bc596e5d8
tracer-pa2018 <tracer@njuics.org>
2018-06-02 01:41:59 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  01:41:59 up 4 days, 16:00, 12 users,  load average: 0.94, 0.99, 0.85 dbaa057e61ea9be6a882016e747de2fb8e98f5

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -2,31 +2,13 @@
 
 make_instr_func(leave) {
 	// esp = ebp
-	// cpu.esp = cpu.ebp;
-	print_reg();
+	cpu.esp = cpu.ebp;
+	// print_reg();
 	
-	int n=5;
-	OPERAND temp, change_esp;
-	temp.data_size = change_esp.data_size = data_size;
-	temp.type = OPR_MEM;
-	change_esp.type = OPR_REG;
-	temp.addr = REG_ESP;
-	change_esp.addr = REG_ESP;
-	temp.sreg = SREG_SS;
-	temp.sreg = SREG_DS;
-	while(n--) {
-		printf("n = %d\n", n);
-		operand_read(&temp);
-		printf("esp = 0x%x, esp store = 0x%x\n", cpu.esp, temp.val);
-		operand_read(&change_esp);
-		change_esp.val -= 4;
-		operand_write(&change_esp);
-	}
-
 	OPERAND old_ebp;
 	old_ebp.data_size = data_size;
-	old_ebp.type = OPR_MEM;
-	old_ebp.addr = REG_ESP;
+	old_ebp.type = OPR_IMM;
+	old_ebp.addr = cpu.esp;
 	old_ebp.sreg = SREG_SS;
 	operand_read(&old_ebp);
 	
