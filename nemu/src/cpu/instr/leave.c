24f94b9716b1a38366a1e915c9aa0bb8a884b4d3
tracer-pa2018 <tracer@njuics.org>
2018-06-01 23:08:24 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:08:24 up 4 days, 13:27, 12 users,  load average: 0.05, 0.09, 0.16 201b3bf3c7eae7c59092caabd12493cbf3be52fc

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -1,25 +1,11 @@
 #include "cpu/instr.h"
 
 make_instr_func(leave) {
-#ifdef DEBUG
-	printf("\nleave start...\n");
-	printf("before leave cpu.eip = 0x%x, ebp = 0x%x, esp = 0x%x\n", cpu.eip, cpu.ebp, cpu.esp);
+	printf("before leave: ");
 	print_reg();
-#endif
 	// esp = ebp
 	cpu.esp = cpu.ebp;
 
-// BUG LIES IN THE OPERAND TYPE AND OPERAND_READ !!!
-#ifdef DEBUG
-	OPERAND temp1, temp2;
-	temp1.data_size = temp2.data_size = data_size;
-	temp1.addr = REG_ESP;
-	temp2.addr = REG_EBP;
-	temp1.type = temp2.type = OPR_MEM;
-	operand_read(&temp1);
-	operand_read(&temp2);
-	printf("leave ebp store = 0x%x, ebp = 0x%x, esp = 0x%x\n", temp2.val, cpu.ebp, cpu.esp);
-#endif
 	OPERAND old_ebp;
 	old_ebp.data_size = data_size;
 	old_ebp.type = OPR_MEM;
@@ -29,6 +15,8 @@ make_instr_func(leave) {
 	cpu.ebp = old_ebp.val;
 	cpu.esp += data_size / 8;
 	
+	printf("After leave:\n");
+	print_reg();
 	return 1;
 }
 
