ca03fc8c53bd27170dc11f178992827d3b9b0510
tracer-pa2018 <tracer@njuics.org>
2018-06-01 23:09:05 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:09:05 up 4 days, 13:27, 12 users,  load average: 0.23, 0.14, 0.17 255da9b18958af446359acd4577160a4d6e72fe9

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -5,18 +5,6 @@ make_instr_func(ret_near) {
 	pop_eip.data_size = data_size;
 	pop_eip.type = OPR_MEM;
 
-#ifdef DEBUG
-	printf("before ret esp = 0x%x\n", cpu.esp);
-	OPERAND tmp1, tmp2;
-	tmp1.data_size = tmp2.data_size = data_size;
-	tmp1.addr = REG_ESP;
-	tmp2.addr = REG_EBP;
-	tmp1.type = tmp2.type = OPR_MEM;
-	operand_read(&tmp1);
-	operand_read(&tmp2);	
-	printf("before ret ebp store = 0x%x, esp store = 0x%x\n", tmp2.val, tmp1.val);
-#endif
-
 	if (data_size == 16) {
 		pop_eip.addr = REG_SP;
 		operand_read(&pop_eip);
@@ -30,20 +18,6 @@ make_instr_func(ret_near) {
 		cpu.esp += 4;
 		cpu.eip = pop_eip.val;
 	}
-
-#ifdef DEBUG	
-	printf("after ret eip = 0x%x, cpu.esp = 0x%x\n", cpu.eip, cpu.esp);
-	OPERAND temp1, temp2;
-	temp1.data_size = temp2.data_size = data_size;
-	temp1.addr = REG_ESP;
-	temp2.addr = REG_EBP;
-	temp1.type = temp2.type = OPR_MEM;
-	operand_read(&temp1);
-	operand_read(&temp2);	
-	printf("ret ebp store = 0x%x, esp store = 0x%x\n", temp2.val, temp1.val);
-	// while(temp1.val == 0x30010)
-	//	;
-#endif	
 
 	return 0;
 }
