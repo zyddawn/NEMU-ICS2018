f38d32f6b23454ed964f67f96f851f4685e8ea03
tracer-pa2018 <tracer@njuics.org>
2018-06-01 18:40:04 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  18:40:04 up 4 days,  8:58, 12 users,  load average: 0.00, 0.02, 0.00 3982e7d4d68022e737e5d3d9181c9b7c2a2b700a

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -11,9 +11,8 @@ make_instr_func(call_near_r_v) {
 	// read call address
 	operand_read(&disp);
 	eip += (1 + data_size / 8);
-#ifdef DEBUG
-	while(1)
-		printf("before CALL esp = 0x%x\n", cpu.esp);
+#ifdef xx
+	printf("before CALL esp = 0x%x\n", cpu.esp);
 #endif
 	// push eip
 	if (data_size == 16) {
