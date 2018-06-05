190f2d22b5b1d4c1c5de351eb363679100f3b544
tracer-pa2018 <tracer@njuics.org>
2018-06-05 14:38:48 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  14:38:48 up 5 days,  7:30, 13 users,  load average: 0.00, 0.01, 0.00 727999d5416deb9bb0f47b6eecc95756ef74b7b5

--- nemu/include/cpu/instr/and.h
+++ nemu/include/cpu/instr/and.h
@@ -6,9 +6,9 @@ make_instr_func(and_i2a_v);
 make_instr_func(and_i2rm_b);
 make_instr_func(and_i2rm_v);
 make_instr_func(and_r2rm_b);
-make_instr_func(and_r2rm_b);
+make_instr_func(and_r2rm_v);
 make_instr_func(and_rm2r_b);
-make_instr_func(and_rm2r_b);
+make_instr_func(and_rm2r_v);
 make_instr_func(and_si2rm_bv);
 
 #endif
