721c1ea6d05128cb3332996713dd0e935463999e
tracer-pa2018 <tracer@njuics.org>
2018-06-06 14:26:54 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  14:26:54 up 5 days, 18:23, 19 users,  load average: 0.02, 0.05, 0.01 8e4c5fdf37e1d2946ed2acf5297efbb961a3ef96

--- nemu/include/cpu/instr/inc.h
+++ nemu/include/cpu/instr/inc.h
@@ -1,7 +1,7 @@
 #ifndef __INSTR_INC_H__
 #define __INSTR_INC_H__
 
-make_instr_func(inc_rm_b);
+// make_instr_func(inc_rm_b);
 make_instr_func(inc_rm_v);
 make_instr_func(inc_r_v);
 
--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -3,13 +3,15 @@
 static void instr_execute_1op() {
 	operand_read(&opr_src);
 	print_reg();
+	uint32_t temp_CF = cpu.eflags.CF;
 	opr_src.val = alu_add(opr_src.val, 1);
+	cpu.eflags.CF = temp_CF;
 	printf("after inc:\n");
 	operand_write(&opr_src);
 	print_reg();
 }
 
-make_instr_impl_1op(inc, rm, b)
+// make_instr_impl_1op(inc, rm, b)
 make_instr_impl_1op(inc, rm, v)
 make_instr_impl_1op(inc, r, v)
 
