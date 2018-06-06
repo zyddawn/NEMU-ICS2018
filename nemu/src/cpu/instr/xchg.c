a4f467d2f29a24aa1468474941898b8eaefc5bbc
tracer-pa2018 <tracer@njuics.org>
2018-06-06 15:25:55 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:25:55 up 5 days, 19:22, 21 users,  load average: 0.00, 0.00, 0.00 3fb3ed9a69c424c40de5b70819800e375c0bc

--- nemu/src/cpu/instr/xchg.c
+++ nemu/src/cpu/instr/xchg.c
@@ -3,24 +3,20 @@
 static void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	opr_dest.val = alu_add(opr_src.val, opr_dest.val);
+	uint32_t temp = opr_src.val;
+	opr_src.val = opr_dest.val;
+	opr_dest.val = temp;
+	operand_write(&opr_src);
 	operand_write(&opr_dest);
 }
 
-static void instr_execute_si2rm_bv() {
-	operand_read(&opr_src);
-	operand_read(&opr_dest);
-	opr_dest.val = alu_add(sign_ext(opr_src.val, 8), opr_dest.val);
-	operand_write(&opr_dest);
-}
 
-make_instr_impl_2op(add, i, rm, v)
-make_instr_impl_2op(add, i, rm, b)
-make_instr_impl_2op(add, i, a, b)
-make_instr_impl_2op(add, i, a, v)
-make_instr_impl_2op(add, r, rm, b)
-make_instr_impl_2op(add, r, rm, v)
-make_instr_impl_2op(add, rm, r, b)
-make_instr_impl_2op(add, rm, r, v)
-make_SignExt_impl_si2rm_bv(add, si, rm, bv)
+make_instr_impl_2op(xchg, i, rm, v)
+make_instr_impl_2op(xchg, i, rm, b)
+make_instr_impl_2op(xchg, i, a, b)
+make_instr_impl_2op(xchg, i, a, v)
+make_instr_impl_2op(xchg, r, rm, b)
+make_instr_impl_2op(xchg, r, rm, v)
+make_instr_impl_2op(xchg, rm, r, b)
+make_instr_impl_2op(xchg, rm, r, v)
 
