d34b54226d11ca2bfa149eff96291accd7b843bf
tracer-pa2018 <tracer@njuics.org>
2018-06-06 15:48:29 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:48:29 up 5 days, 19:44, 21 users,  load average: 0.04, 0.05, 0.00 40453672351be7046bf6a8c0845c78e46512769

--- nemu/include/cpu/instr/pop.h
+++ nemu/include/cpu/instr/pop.h
@@ -4,14 +4,14 @@
 make_instr_func(pop_r_v);
 make_instr_func(pop_rm_v);
 
-/* make_instr_func(pop_eax_v);
+make_instr_func(pop_eax_v);
 make_instr_func(pop_ecx_v);
 make_instr_func(pop_edx_v);
 make_instr_func(pop_ebx_v);
 make_instr_func(pop_esp_v);
 make_instr_func(pop_ebp_v);
 make_instr_func(pop_esi_v);
-make_instr_func(pop_edi_v); */
+make_instr_func(pop_edi_v); 
 
 #endif
 
--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -1,9 +1,24 @@
 #include "cpu/instr.h"
 
+static void instr_execute_1op() {
+	opr_dest.data_size = 32;
+	opr_dest.type = OPR_MEM;
+	opr_dest.addr = cpu.esp;
+	operand_read(&opr_dest);
+	if(data_size == 8)
+		opr_dest.val &= 0xFF;
+	else if(data_size == 16)
+		opr_dest.val &= 0xFFFF;
+	opr_src.val = opr_dest.val;
+	operand_write(&opr_src);
+	cpu.esp += 4;
+}
+
+make_instr_impl_1op(pop, r, v)
+make_instr_impl_1op(pop, rm, v)
 
 
-
-/* static void instr_execute() {
+static void instr_execute() {
 	cpu.esp += 4;
 }
 
@@ -14,11 +29,7 @@ pop_REG_helper(ebx, BX)
 pop_REG_helper(esp, SP)
 pop_REG_helper(ebp, BP)
 pop_REG_helper(esi, SI)
-pop_REG_helper(edi, DI) */
-
-
-
-
+pop_REG_helper(edi, DI)
 
 
 
