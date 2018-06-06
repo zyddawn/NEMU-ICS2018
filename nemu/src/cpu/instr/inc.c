b89d120d202cff9515c2308d1e9525f0da0c9435
tracer-pa2018 <tracer@njuics.org>
2018-06-05 21:42:22 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:42:22 up 5 days, 14:34, 16 users,  load average: 0.23, 0.13, 0.05 e91188ca32b320c1c75edaba4761cdae7bbf2c0

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -2,8 +2,11 @@
 
 static void instr_execute_1op() {
 	operand_read(&opr_src);
+	print_reg();
 	opr_src.val = alu_add(opr_src.val, 1);
+	printf("after inc:\n");
 	operand_write(&opr_src);
+	print_reg();
 }
 
 make_instr_impl_1op(inc, rm, b)
