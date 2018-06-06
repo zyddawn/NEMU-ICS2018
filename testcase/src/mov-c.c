2be013ff02d2bf299062819da3ada9895c4156f6
tracer-pa2018 <tracer@njuics.org>
2018-06-05 23:13:58 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:13:58 up 5 days, 16:05, 16 users,  load average: 0.01, 0.02, 0.00 8ab153f59e97bed44143e3db1f833853eeb026ff

--- testcase/src/mov-c.c
+++ testcase/src/mov-c.c
@@ -5,21 +5,35 @@ int b;
 
 int main() {
 	A[0] = 0;
+	printf("0\n");
 	A[1] = 1;
+	printf("1\n");
 	A[2] = 2;
+	printf("2\n");
 	A[3] = 3;
+	printf("3\n");
 	A[4] = 4;
-
-	b = A[3];
-	A[5] = b;
+	printf("4\n");
 	
+	b = A[3];
+	printf("5\n");
+	A[5] = b;
+	printf("6\n");
+
 	nemu_assert(A[0] == 0);
+	printf("7\n");
 	nemu_assert(A[1] == 1);
+	printf("8\n");
 	nemu_assert(A[2] == 2);
+	printf("9\n");
 	nemu_assert(A[3] == 3);
+	printf("10\n");
 	nemu_assert(A[4] == 4);
+	printf("11\n");
 	nemu_assert(b == 3);
+	printf("12\n");
 	nemu_assert(A[5] == 3);
+	printf("13\n");
 
 	HIT_GOOD_TRAP;
 
