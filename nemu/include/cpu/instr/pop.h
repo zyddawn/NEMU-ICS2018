9803172aefb91fbb98323e810870c60eb9e77f8b
tracer-pa2018 <tracer@njuics.org>
2018-06-06 15:46:08 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:46:08 up 5 days, 19:42, 21 users,  load average: 0.07, 0.04, 0.00 2474c6037547195ff938e3e3ce57e38b8e4d9c09

--- nemu/include/cpu/instr/pop.h
+++ nemu/include/cpu/instr/pop.h
@@ -1,7 +1,6 @@
 #ifndef __INSTR_POP_H__
 #define __INSTR_POP_H__
 
-
 make_instr_func(pop_r_v);
 make_instr_func(pop_rm_v);
 
@@ -14,5 +13,5 @@ make_instr_func(pop_ebp_v);
 make_instr_func(pop_esi_v);
 make_instr_func(pop_edi_v); */
 
-
 #endif
+
