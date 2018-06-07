53db9b182009258c2cce10279f3dc5b3632e6906
tracer-pa2018 <tracer@njuics.org>
2018-06-07 11:15:16 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  11:15:16 up 6 days,  5:15, 23 users,  load average: 0.00, 0.00, 0.00 1c75f874f134878558262de44e440bf8959b1672

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -48,6 +48,7 @@ extern uint8_t data_size;
 #include "cpu/instr/mul.h"
 #include "cpu/instr/imul.h"
 #include "cpu/instr/idiv.h"
+#include "cpu/instr/stos.h"
 
 
 #endif
---
+++ nemu/include/cpu/instr/stos.h
@@ -0,0 +1,7 @@
+#ifndef __INSTR_STOS_H__
+#define __INSTR_STOS_H__
+
+make_instr_func(stos_v);
+make_instr_func(stos_b);
+
+#endif
