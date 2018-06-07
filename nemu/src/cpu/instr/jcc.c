05201f5af4093d4969d2814adc4f7964ca29f4a3
tracer-pa2018 <tracer@njuics.org>
2018-06-07 02:04:12 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  02:04:12 up 6 days,  3:52, 23 users,  load average: 0.13, 0.07, 0.01 5abcc18092260ee0fce3fc3a75a9cc6c4c459d50

--- nemu/src/cpu/instr/jcc.c
+++ nemu/src/cpu/instr/jcc.c
@@ -3,9 +3,10 @@
 static void instr_execute_1op_cc_pass() {
 	operand_read(&opr_src);
 	cpu.eip += sign_ext(opr_src.val, opr_src.data_size);
+	print_flags();
 }
 
-static void instr_execute_1op_cc_fail() {/* Do nothing */}
+static void instr_execute_1op_cc_fail() {print_flags(); /* Do nothing */}
 
 #define jcc_helper(suffix) \
 	make_instr_impl_1op_cc(j, i, suffix, e) \
