bd82ef1c8b4ac721742cb17ba514de1542696dae
tracer-pa2018 <tracer@njuics.org>
2018-06-07 00:20:44 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  00:20:44 up 6 days,  2:27, 23 users,  load average: 0.08, 0.03, 0.01 a0e5910ed62966fde0d8eaffe2df41e15ee30445

--- testcase/src/max.c
+++ testcase/src/max.c
@@ -16,7 +16,6 @@ int main() {
 	int i, j, ans_idx = 0;
 	for(i = 0; i < NR_DATA; i ++) {
 		for(j = 0; j < NR_DATA; j ++) {
-			printf("i = %d, j = %d, gt = %d\n", test_data[i], test_data[j], ans[ans_idx]);
 			nemu_assert(max(test_data[i], test_data[j]) == ans[ans_idx ++]);
 		}
 	}
