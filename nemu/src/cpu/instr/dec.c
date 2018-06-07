fa2dc13ea965b45653c44d9cfc7ff1a35e07e88f
Yudong Zhang <141120159@smail.nju.edu.cn>
2018-06-06 20:06:20 -0700

    Fixed bugs in dec instruction

--- nemu/src/cpu/instr/dec.c
+++ nemu/src/cpu/instr/dec.c
@@ -3,11 +3,11 @@
 static void instr_execute_1op() {
 	operand_read(&opr_src);
 	uint32_t temp_CF = cpu.eflags.CF;
-	printf("Before dec: 0x%x\n", opr_src.val);
+	// printf("Before dec: 0x%x\n", opr_src.val);
 	opr_src.val = alu_sub(1, opr_src.val);
 	cpu.eflags.CF = temp_CF;
 	operand_write(&opr_src);
-	printf("after dec: 0x%x\n", opr_src.val);
+	// printf("after dec: 0x%x\n", opr_src.val);
 }
 
 // make_instr_impl_1op(dec, rm, b)
