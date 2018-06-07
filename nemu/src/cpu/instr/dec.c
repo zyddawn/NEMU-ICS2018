b64f6168837cc53953291dc3b632adbf49432baf
tracer-pa2018 <tracer@njuics.org>
2018-06-06 20:05:35 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:05:35 up 5 days, 22:12, 22 users,  load average: 0.00, 0.02, 0.00 1b00ab3488caa03ebc241eb8cadb0ec31737d4b

--- nemu/src/cpu/instr/dec.c
+++ nemu/src/cpu/instr/dec.c
@@ -4,7 +4,7 @@ static void instr_execute_1op() {
 	operand_read(&opr_src);
 	uint32_t temp_CF = cpu.eflags.CF;
 	printf("Before dec: 0x%x\n", opr_src.val);
-	opr_src.val = alu_sub(opr_src.val, 1);
+	opr_src.val = alu_sub(1, opr_src.val);
 	cpu.eflags.CF = temp_CF;
 	operand_write(&opr_src);
 	printf("after dec: 0x%x\n", opr_src.val);
