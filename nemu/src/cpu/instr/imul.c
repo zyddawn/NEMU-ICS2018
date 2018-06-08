ab629c9b559d3831aaccaf17db792d1514b425bb
tracer-pa2018 <tracer@njuics.org>
2018-06-07 22:15:25 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:15:25 up 6 days, 12:23, 22 users,  load average: 0.10, 0.05, 0.01 dbf9a2824cee3765be380ade8e047c069b448b44

--- nemu/src/cpu/instr/imul.c
+++ nemu/src/cpu/instr/imul.c
@@ -1,34 +1,60 @@
 #include "cpu/instr.h"
 
-#define imul_helper(suffix) \
-	make_instr_func(concat(imul_rm2a_, suffix)) { \
-		int len = 1; \
-		concat(decode_data_size_, suffix) \
-		decode_operand_rm \
-		operand_read(&opr_src); \
-		uint32_t temp = cpu.eax; \
-		opr_dest.type = OPR_REG; \
-		opr_dest.addr = REG_EAX; \
-		if (opr_src.data_size == 8) \
-			temp &= 0xFF; \
-		else if (opr_src.data_size == 16) \
-			temp &= 0xFFFF; \
-		uint64_t res = alu_imul(opr_src.val, temp, opr_src.data_size); \
-		opr_dest.val = res; \
-		operand_write(&opr_dest); \
-		if (opr_src.data_size == 8 && ((res >> 8) & 0xFF) == 0) { \
-			cpu.eflags.CF = cpu.eflags.OF = 0;} \
-		else if (opr_src.data_size == 16 && ((res >> 16) & 0xFFFF) == 0) { \
-			cpu.eflags.CF = cpu.eflags.OF = 0;} \
-		else if (opr_src.data_size == 32 && ((res >> 32) & 0xFFFFFFFF) == 0) { \
-			cpu.eflags.CF = cpu.eflags.OF = 0;} \
-		else { \
-			cpu.eflags.CF = cpu.eflags.OF = 1;} \
-		return len; \
-	}
+make_instr_func(imul_rm2a_b) {
+	int len = 1;
+	OPERAND opr_src, ax;
+	opr_src.data_size = 8;
+	decode_operand_rm
+	operand_read(&opr_src); 
+	uint32_t al = cpu.eax & 0xFF;
+	ax.data_size = 16;
+	ax.type = OPR_REG;
+       	ax.addr = REG_AX;	
+	uint64_t res = alu_imul(opr_src.val, al, 8); 
+	ax.val = res & 0xFFFF;
+	operand_write(&ax);
+	if ((res >> 8) & 0xFF == 0)
+		cpu.eflags.CF = cpu.eflags.OF = 0;
+	else
+		cpu.eflags.CF = cpu.eflags.OF = 1;
+	return len;
+}
 
-imul_helper(b)
-imul_helper(v)
+
+make_instr_func(imul_rm2a_v) {
+	int len = 1;
+	OPERAND opr_src, eax, edx;
+	decode_data_size_v
+	decode_operand_rm
+	operand_read(&opr_src); 
+	uint32_t num;
+	uint64_t res;
+	eax.data_size = edx.data_size = data_size;
+	eax.type = edx.type = OPR_REG;
+	eax.addr = REG_EAX;
+	edx.addr = REG_EDX;
+	num = cpu.eax;
+       	if (data_size == 16) {
+		num &= 0xFFFF;
+		res = alu_imul(opr_src.val, num, data_size);
+		eax.val = res & 0xFFFF;
+		edx.val = (res >> 16) & 0xFFFF; }
+	else {
+		res = alu_imul(opr_src.val, num, data_size);
+		eax.val = res & 0xFFFFFFFF;
+		edx.val = (res >> 32) & 0xFFFFFFFF;
+	}
+	operand_write(&eax);
+	operand_write(&edx);
+	if (data_size == 16 && (res >> 16) & 0xFFFF == 0)
+		cpu.eflags.CF = cpu.eflags.OF = 0;
+	else if (data_size == 32 && (res >> 32) == 0)
+		cpu.eflags.CF = cpu.eflags.OF = 0;
+	else
+		cpu.eflags.CF = cpu.eflags.OF = 1;
+	return len;
+}
+
 
 
 make_instr_func(imul_rm2r_v) {
