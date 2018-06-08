148788e4df5e2f36bd631005cde2c611832e62e7
tracer-pa2018 <tracer@njuics.org>
2018-06-07 21:04:33 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:04:33 up 6 days, 11:12, 22 users,  load average: 0.00, 0.00, 0.00 708c74eb44761ea3a28ad2352948b809de8ce61

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -14,7 +14,7 @@ uint32_t hw_mem_read(paddr_t paddr, size_t len) {
 	// if(paddr == cpu.ebp-4 && hw_mem[cpu.ebp-4] == 3)
 	//	printf("[ebp-4] = %d\n", hw_mem[cpu.ebp-4]);
 	if(paddr == cpu.ebp + 8)
-		printf("[ebp+8] = %d\n", hw_mem[cpu.ebp+8]);
+		printf("[ebp+8] = %d\n", hw_mem[cpu.ebp+8]+256*hw_mem[cpu.ebp+9]);
 	return ret;
 }
 
