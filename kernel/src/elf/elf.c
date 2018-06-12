21fb3cbc5f0d819d64f5de598ec80e8f874edb3b
tracer-pa2018 <tracer@njuics.org>
2018-06-12 01:01:54 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  01:01:54 up 7 days, 18:56, 12 users,  load average: 0.00, 0.01, 0.00 18902a412d26806781b3a5226813046ac127c141

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -39,7 +39,7 @@ uint32_t loader() {
 			memcpy((void *)elf + ph->p_offset, (void *)elf + ph->p_vaddr, ph->p_filesz); 
 
 			/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
-			memset((void *) elf + ph->p_vaddr + ph->filesz, 0, ph->memsz - ph->p_filesz);
+			memset((void *) elf + ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
