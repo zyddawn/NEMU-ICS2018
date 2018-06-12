664e7ecdb90f0a30a3356dc66d90fb26e928ea60
tracer-pa2018 <tracer@njuics.org>
2018-06-12 12:26:43 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  12:26:43 up 7 days, 21:06, 12 users,  load average: 0.00, 0.02, 0.00 3dbef16f4c5227ce935c9ee380ecbcdc4ef2a242

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,7 +36,7 @@ uint32_t loader() {
 		if(ph->p_type == PT_LOAD) {
 			// panic("Please implement the loader");
 			/* TODO: copy the segment from the ELF file to its proper memory area */
-			memcpy((ph->p_vaddr), elf + ph->p_offset, ph->p_filesz);
+			memcpy((void *)ph->p_vaddr, elf + ph->p_offset, ph->p_filesz);
 			// memcpy((void *) (elf + ph->p_offset), get_mem_addr() + ph->p_vaddr, ph->p_filesz);
 			/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
 			memset(ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);  // BUG
