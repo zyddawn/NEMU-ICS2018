a2f9cfa8cd1b079683ec40d36f5ee4aece112922
tracer-pa2018 <tracer@njuics.org>
2018-06-12 13:58:51 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  13:58:51 up 7 days, 21:44, 18 users,  load average: 0.00, 0.00, 0.00 c1815c227dce6d593449960299850369607a59

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -14,40 +14,38 @@ make_instr_func(call_near_r_v) {
 	operand_read(&disp);
 	eip += (1 + data_size / 8);
 	// push eip
-	/*if (data_size == 16) {
-		cpu.esp -= 2;
-		push_eip.val = eip & 0x0000FFFF;
-		operand_write(&push_eip);
-		cpu.eip = (eip + disp.val) & 0x0000FFFF;
-	}
-	else {*/
 	cpu.esp -= 4;
 	push_eip.addr = cpu.esp;
 	push_eip.val = eip;
 	operand_write(&push_eip);
 	cpu.eip = eip + disp.val;
-	
-	/* printf("call pushed eip = 0x%x, current eip = 0x%x\n", push_eip.val, cpu.eip);
-	print_reg();
-	}
-	
-	OPERAND temp;
-	temp.data_size = 32;
-	temp.type = OPR_MEM;
-	temp.addr = cpu.esp;
-	temp.sreg = SREG_SS;
-	operand_read(&temp);
-	printf("after call check esp = 0x%x, esp store = 0x%x\n\n", cpu.esp, temp.val); */
-	
 	return 0;
 }
 
-// make_instr_func(call_near_rm_v) {
-	
+make_instr_func(call_near_rm_v) {
+	OPERAND opr_src, rm, push_eip;
+	opr_src.data_size = data_size;
+	decode_operand_r
+	operand_read(&opr_src);
+	printf("opr_src.val = 0x%x, eax = 0x%x\n", opr_src.val, cpu.eax);
+	rm.data_size = push_eip.data_size = data_size;
+	rm.type = OPR_MEM;
+	rm.addr = opr_src.va;;
+	rm.sreg = SREG_CS;
+	push_eip.type = OPR_MEM;
+	push_eip.sreg = SREG_SS;
 
-
-
-
-
-//}
+	// read call address
+	operand_read(&rm);
+	eip += (1 + data_size / 8);
+	// push eip
+	cpu.esp -= 4;
+	push_eip.addr = cpu.esp;
+	push_eip.val = eip;
+	operand_write(&push_eip);
+	cpu.eip = rm.val;
+	while (1)
+		;
+	return 0;
+}
 
