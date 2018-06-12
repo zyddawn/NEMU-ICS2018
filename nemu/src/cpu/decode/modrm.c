38e912636705eb84f524198102d4643c2490fa3b
tracer-pa2018 <tracer@njuics.org>
2018-06-12 12:41:31 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  12:41:31 up 7 days, 21:21, 17 users,  load average: 0.07, 0.02, 0.00 736c5e1317864a6c7eb39fd6824d2f177ff87a1c

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -129,8 +129,8 @@ int modrm_opcode_rm(uint32_t eip, uint8_t * opcode, OPERAND * rm) {
 int modrm_opcode(uint32_t eip, uint8_t * opcode) {
         MODRM modrm;
         modrm.val = instr_fetch(eip, 1);
-        printf("modrm.val = 0x%x\n", modrm.val);
 	*opcode = modrm.reg_opcode;
+	printf("opcode = 0x%x, *opcode = 0x%x\n", opcode, *opcode);
         return 1;
 }
 
