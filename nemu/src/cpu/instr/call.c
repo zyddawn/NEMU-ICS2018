8129c81bfa460384661d06626b2fe09e07494e3f
tracer-pa2018 <tracer@njuics.org>
2018-06-14 17:47:40 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  17:47:40 up 8 days,  2:55, 23 users,  load average: 0.00, 0.00, 0.00 2681bf15515692ef6d52be0e64d9ff585e9b2f43

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -43,7 +43,7 @@ make_instr_func(call_near_rm_v) {
 	// printf("hw_mem[0x30000]: %02x %02x %02x %02x %02x %02x\n", hw_mem[0x30000], hw_mem[0x30001], hw_mem[0x30002], hw_mem[0x30003], hw_mem[0x30004], hw_mem[0x30005]);
 	// printf("hw_mem[0x60000]: %02x %02x %02x %02x %02x %02x\n", hw_mem[0x60000], hw_mem[0x60001], hw_mem[0x60002], hw_mem[0x60003], hw_mem[0x60004], hw_mem[0x60005]);
 	// printf("hw_mem[0x38504]: %02x %02x %02x %02x %02x %02x\n", hw_mem[0x38504], hw_mem[0x38505], hw_mem[0x38506], hw_mem[0x38507], hw_mem[0x38508], hw_mem[0x38509]);
-	for(int i=0x30000; i<0x30100; ++i) {
+	for(int i=0x38000; i<0x38100; ++i) {
 		if(i % 16 == 0)
 			printf("\n0x%08x: ", i);
 		printf("%02x ", hw_mem[i]);
