4f57629775444628e3875d2b7be0b94560d78d9f
tracer-pa2018 <tracer@njuics.org>
2018-06-16 01:38:44 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  01:38:44 up 8 days, 18:39, 11 users,  load average: 0.01, 0.04, 0.02 3343aab7822ca52e7118f7d9be738542ffa6d8fe

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -42,7 +42,7 @@ make_instr_func(jmp_short_) {
 	// int offset = sign_ext(rel.val, data_size);
 	// print_asm_1("jmp", "",  1 + data_size / 8, &rel);
 	char offset = rel.val & 0xFF;  // signed 8-bit
-	printf("Before jmp, eip = 0x%x (%d), disp = 0x%x (%d)\n", cpu.eip, cpu.eip, offset, offset);
+	printf("Before jmp, eip = 0x%x (%d), disp = 0x%x (%d), re.val = 0x%x\n", cpu.eip, cpu.eip, offset, offset, rel.val);
 	cpu.eip += offset;  
 	printf("After jmp, eip = 0x%x (%d)\n", cpu.eip, cpu.eip);
         return 2;
