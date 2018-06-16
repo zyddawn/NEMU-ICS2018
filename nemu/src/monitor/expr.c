893e8bcba7a6f4846f91aa3002652aa990caeff9
tracer-pa2018 <tracer@njuics.org>
2018-06-15 23:09:42 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:09:42 up 8 days, 16:10, 11 users,  load average: 0.23, 0.13, 0.04 95a518e5b79b2dff2c6e27b7c2cdc4a396e1493d

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -322,6 +322,7 @@ long long int eval(int p, int q, bool *success) {
 		int op = dominant_op(p, q);
 		if (op == p) {
 			val2 = eval(op + 1, q, success);
+			printf("val2 = %lld, op=%d, \n", val2, tokens[p].type);
 			if((val2!=(long long int)int_max+1 || tokens[op].type!=NEG) && (out_of_int_range(val2))) {
 				printf("Error! Exceeded int range.\n");
 				*success = false;
