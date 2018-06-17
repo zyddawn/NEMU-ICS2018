2a96fb20a025dc02204421ce0e7fe42ea2a2d0af
Yudong Zhang <141120159@smail.nju.edu.cn>
2018-06-16 20:37:58 -0700

    Add div instruction to opcode map

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -47,6 +47,7 @@ extern uint8_t data_size;
 #include "cpu/instr/setcc.h"
 #include "cpu/instr/mul.h"
 #include "cpu/instr/imul.h"
+#include "cpu/instr/div.h"
 #include "cpu/instr/idiv.h"
 #include "cpu/instr/stos.h"
 #include "cpu/instr/cmps.h"
