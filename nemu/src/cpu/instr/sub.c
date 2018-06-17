e7515f3422521b54893a1db2d172cbf252ba1ed0
tracer-pa2018 <tracer@njuics.org>
2018-06-16 22:21:27 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:21:27 up 8 days, 19:27,  5 users,  load average: 0.09, 0.03, 0.01 845c1b2cfbe8d01a83560a523666a342d525266b

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -1,6 +1,7 @@
 #include "cpu/instr.h"
 
-static void instr_execute_2op() {
+// static 
+void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 	opr_dest.val = alu_sub(opr_src.val, opr_dest.val);
