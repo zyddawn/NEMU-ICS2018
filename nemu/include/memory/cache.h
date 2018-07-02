10c22b182dc275d363007cc17ad7d4c3efc20eca
Yudong Zhang <141120159@smail.nju.edu.cn>
2018-07-02 00:43:11 -0700

    Add functions get_tag(), get_set_index(), get_block_index()

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -3,19 +3,23 @@
 
 #include "nemu.h"
 
-#define BLOCK_SIZE 64
+#define BLOCK_SIZE_B 64
 #define CACHE_LINES 1024
+#define TAG_BITS 19
+#define SET_INDEX_BITS 7
+#define BLOCK_INDEX_BITS 6
+
 // physics addr 32 bits in total
-// 16 (tag) + 10 (line index) + 6 (block index)
+//  19 (tag) + 7 (set index) + 6 (block index)
 
 typedef struct {
 	bool valid_bit;
 	uint32_t tag;
-	uint8_t block_data[BLOCK_SIZE];
+	uint8_t block_data[BLOCK_SIZE_B];
 } BLOCK;
 
 // 8-way set associative
-extern BLOCK cache[CACHE_LINES/8][8];
+extern BLOCK cache[][8];
 
 
 
--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -3,9 +3,29 @@
 #include <memory.h>
 #include <stdio.h>
 
+BLOCK cache[CACHE_LINES/8][8];
+
+static uint32_t get_tag(paddr_t paddr) {
+	uint32_t res = (uint32_t) paddr;
+	return (res >> (SET_INDEX_BITS + BLOCK_INDEX_BITS));
+}
+
+static uint32_t get_set_index(paddr_t paddr) {
+	uint32_t res = (uint32_t) paddr;
+	return ((res >> BLOCK_INDEX_BITS) & 0x7F);
+}
+
+static uint32_t get_block_index(paddr_t paddr) {
+	uint32_t res = (uint32_t) paddr;
+	return (res & 0x3F);
+}
+
 
 uint32_t cache_read(paddr_t paddr, size_t len, ) {
-	
+	bool cache_hit = false;
+		
+
+
 
 
 }
