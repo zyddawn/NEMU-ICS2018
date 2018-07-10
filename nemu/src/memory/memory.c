08118cb0d30e09512e8882c7217fcfe617bca894
tracer-pa2018 <tracer@njuics.org>
2018-07-09 23:21:32 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:21:32 up 11:06, 11 users,  load average: 0.22, 0.10, 0.06 74bda86a68232524df24c6ce50e369fef80a37

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -10,7 +10,7 @@ uint8_t hw_mem[MEM_SIZE_B];
 
 uint32_t hw_mem_read(paddr_t paddr, size_t len) {
 	uint32_t ret = 0;
-	if (paddr > 0xf0000000)
+	if (cpu.eip == 0x30200 || cpu.eip == 0x30265)
 		printf("hw_mem: EIP = 0x%x, paddr = 0x%x\n", cpu.eip, paddr);
 	memcpy(&ret, hw_mem + paddr, len);
 	return ret;
