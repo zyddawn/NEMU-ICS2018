8741148d27ad60fc1c82d6da1f7cd34314835c43
tracer-pa2018 <tracer@njuics.org>
2018-07-09 22:22:55 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:22:55 up 10:08, 10 users,  load average: 0.10, 0.03, 0.01 96886d35df6781be4b9cbf387b71ebd88fe6f96a

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -18,18 +18,18 @@
 // physics addr 27 bits in total
 //  14 (tag) + 7 (set index) + 6 (block index)
 
-typedef struct {
+typedef struct CacheLine {
 	bool valid_bit;
 	uint32_t tag;
 	uint8_t block_data[BLOCK_SIZE_B];
-} CacheLine;
+};
 
 
 // 8-way set associative
 extern CacheLine L1_dcache[][SET_SIZE];
 
-
 void init_cache(CacheLine cache[][SET_SIZE]);
 uint32_t cache_read(paddr_t paddr, size_t len, CacheLine cache[][SET_SIZE]);
 void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine cache[][SET_SIZE]);
+
 
