c845739a9c25e407b8f6c40ba77cc79a7b78dd99
Yudong Zhang <141120159@smail.nju.edu.cn>
2018-07-11 06:23:13 -0700

    Finish main part of load_sreg()

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -6,6 +6,10 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg) {
 	/* TODO: perform segment translation from virtual address to linear address
 	 * by reading the invisible part of the segment register 'sreg'
 	 */
+	Assert(sreg < 6, "No such sreg.");
+	Assert(cpu.segReg[sreg].ti == 0, "Selecting LDT (not implemented).");
+	Assert((cpu.segReg[sreg].index<<3) < cpu.gdtr.limit, "GDTR range exceeded.");  // seg_desc is 64-bit, thus use 8*index
+	Assert(offset + len < cpu.segReg[sreg].limit, "Segment range exceeded.");
 	return cpu.segReg[sreg].base + offset;
 }
 
@@ -14,6 +18,14 @@ void load_sreg(uint8_t sreg) {
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-	
 
-}
+	SegDesc cur_seg = *(cpu.gdtr.base + (cpu.segReg[sreg].index << 3));
+	uint32_t base = (base_31_24 << 24) | (base_23_16<< 16) | base_15_0, 
+		 limit = (limit_19_16 << 16) | limit_15_0;
+	cpu.segReg[sreg].base = base;
+	cpu.segReg[sreg].limit = limit;
+	
+	// do some checking
+
+
+}	
