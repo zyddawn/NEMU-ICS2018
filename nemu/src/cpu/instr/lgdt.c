ffc8a9e0256077a2a9bd29b7c1989ed3208b9739
Yudong Zhang <141120159@smail.nju.edu.cn>
2018-07-10 22:33:06 -0700

    Implement lgdt

---
+++ nemu/src/cpu/instr/lgdt.c
@@ -0,0 +1,21 @@
+#include "cpu/instr.h"
+
+
+static void instr_execute_1op() {
+	cpu.gdtr.limit = opr_src.val & 0xFFFF;
+	cpu.gdtr.base = opr_src.val >> 16;
+	if (data_size == 16)
+		cpu.gdtr.base &= 0x00FFFFFF;
+		// not sure if it's correct
+}
+
+
+make_instr_impl_1op(lgdt, rm, v)
+
+
+
+
+
+
+
+
