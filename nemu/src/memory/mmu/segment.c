80223a972980f680e67401fc2c51ccf11f6f0a12
tracer-pa2018 <tracer@njuics.org>
2018-07-13 09:47:57 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  09:47:57 up 1 day,  3:06, 18 users,  load average: 0.07, 0.08, 0.03 9413225a66be003ed31b01a65d82d5e6941c6ce0

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -6,12 +6,8 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg) {
 	/* TODO: perform segment translation from virtual address to linear address
 	 * by reading the invisible part of the segment register 'sreg'
 	 */
-	// printf("Segment translate: \n");
-	// printf("sreg = 0x%x\n", sreg);
 	assert(sreg < 6);
-	// printf("ti = %d\n", cpu.segReg[sreg].ti);
 	// assert(cpu.segReg[sreg].ti == 0);
-	// printf("index = 0x%x, limit = 0x%x\n", cpu.segReg[sreg].index<<3, cpu.gdtr.limit);
 	// assert((cpu.segReg[sreg].index<<3) < cpu.gdtr.limit);  // seg_desc is 64-bit, thus use 8*index
 	// load_sreg(sreg);
 	return cpu.segReg[sreg].base + offset;
