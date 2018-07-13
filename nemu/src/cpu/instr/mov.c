6f0348b6c5ba6685628782bd202c27898aa7d05a
Yudong Zhang <141120159@smail.nju.edu.cn>
2018-07-13 09:00:58 -0700

    Fixed bugs in mov_crx2r_v and mov_r2crx_v

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -321,7 +321,6 @@ make_instr_func(mov_crx2r_v) {
 	printf("Calling mov_crx2r_v.\n");
 	int len = 2;
 	uint32_t tmp = instr_fetch(eip + 1, 1);	
-	printf("tmp = 0x%x\n", tmp);
 	int cr_index = (tmp >> 3) & 0x7,
 	    reg_index = tmp & 0x7;
 	OPERAND r;
