113774464761ad09c8cabc103f829b084704812b
Yudong Zhang <141120159@smail.nju.edu.cn>
2018-07-14 04:50:48 -0700

    Finished page_translate()

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -1,15 +1,39 @@
 #include "cpu/cpu.h"
 #include "memory/memory.h"
 
+static uint32_t get_dir(laddr_t laddr)
+	return (laddr >> 22);  // high 10 bits
+
+static uint32_t get_tb(laddr_t laddr) 
+	return (laddr >> 12) & 0x3ff;  // middle 10 bits
+
+static uint32_t get_offset(laddr_t laddr)
+	return (laddr & 0xfff);   // lower 12 bits
+
+
 // translate from linear address to physical address
 paddr_t page_translate(laddr_t laddr) {
 #ifndef TLB_ENABLED
-	/*TODO*/
-	printf("\nPlease implement page_translate()\n");
-	assert(0);
-	// paddr_read()
-
-	// assert(present_bit);  // for debugging
+	uint32_t dir_id = get_dir(laddr),
+		 tb_id = get_tb(laddr),
+		 offset = get_offset(laddr);
+	// fetch PageDir
+	laddr_t dir_addr = (cpu.cr3.pdbr << 12) | (dir_id * 4); // PDBR + dir_index * 4
+	PDE page_dir;
+	page_dir.val = paddr_read(dir_addr, 4);
+	if (!page_dir.present) {
+		printf("PageDir present bit != 1!\n");
+		assert(0);
+	}
+	// fetch PageTable
+	laddr_t tb_addr = (page_dir.page_frame << 12) | (tb_id * 4);
+	PTE page_tb;
+	page_tb.val = paddr_read(tb_addr, 4);
+	if (!page_tb.present) {
+		printf("PageTable present bit != 1!\n");
+		assert(0);
+	}
+	return (page_tb.page_frame << 12) | offset;
 #else	
 	return tlb_read(laddr) | (laddr & PAGE_MASK);;
 #endif
