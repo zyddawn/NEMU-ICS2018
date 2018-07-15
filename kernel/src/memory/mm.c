09fad4d47d52bd61318951e836c2961aa82d853f
tracer-pa2018 <tracer@njuics.org>
2018-07-15 00:39:07 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  00:39:07 up 1 day, 18:14, 10 users,  load average: 0.25, 0.09, 0.02 81b62def8d20fdd87eb46cf8ee5dc04fbe60a356

--- kernel/src/memory/mm.c
+++ kernel/src/memory/mm.c
@@ -27,6 +27,9 @@ void init_mm() {
 	memset(updir, 0, NR_PDE * sizeof(PDE));
 
 	/* create the same mapping above 0xc0000000 as the kernel mapping does */
+	Log("\nCopy kernel");
+	Log("vaddr = 0x%x", kpdir[KOFFSET / PT_SIZE]);
+	Log("paddr = 0x%x", updit[KOFFSET / PT_SIZE]);
 	memcpy(&updir[KOFFSET / PT_SIZE], &kpdir[KOFFSET / PT_SIZE], 
 			(PHY_MEM / PT_SIZE) * sizeof(PDE));
 
