defb2787dfb909bedfd0ad92f12ba592a9fce365
tracer-pa2018 <tracer@njuics.org>
2018-08-11 10:43:45 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:43:45 up  6:53, 18 users,  load average: 0.04, 0.10, 0.05 59235ef251a355f54f7858775840f057d294e3aa

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -14,9 +14,10 @@ void *sbrk(int incr) {
 }
 
 int main() {
+	BREAK_POINT
 	sprintf(buf, "%s", "Hello world!\n");
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
-
+	
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
