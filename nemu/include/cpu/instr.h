26880a9a914641e03f7c70651783d73774b2faf6
tracer-pa2018 <tracer@njuics.org>
2018-08-10 21:30:25 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:30:25 up  3:13, 14 users,  load average: 0.03, 0.02, 0.00 3abebe9c3cb0fc511b646ea21d739cd725bc79

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -55,6 +55,7 @@ extern uint8_t data_size;
 #include "cpu/instr/lidt.h"
 #include "cpu/instr/cli.h"
 #include "cpu/instr/sti.h"
+#include "cpu/instr/int.h"
 
 
 
--- nemu/src/cpu/instr/int.c
+++ nemu/src/cpu/instr/int.c
@@ -1,4 +1,7 @@
 #include "cpu/instr.h"
+#include "cpu/intr.h"
+
+extern void raise_intr(uint8_t intr_no);
 
 make_instr_func(int_i_b) {
 	uint32_t intr_no = instr_fetch(cpu.eip + 1, 1);
