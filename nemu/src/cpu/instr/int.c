afbbf3cd0f3496f9536124ec5a97f2c495c51cc9
tracer-pa2018 <tracer@njuics.org>
2018-08-11 11:07:05 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  11:07:05 up  7:16, 21 users,  load average: 0.12, 0.07, 0.06 9295ee22ed0bc11f83f8518dcc3dea628c6c6b2f

--- nemu/src/cpu/instr/int.c
+++ nemu/src/cpu/instr/int.c
@@ -4,6 +4,7 @@
 extern void raise_sw_intr(uint8_t intr_no);
 
 make_instr_func(int_i_b) {
+	printf("int $0x80\n");
 	uint32_t intr_no = instr_fetch(cpu.eip + 1, 1);
 	// cpu.eip += 2;
 	// react to exception & interruption
