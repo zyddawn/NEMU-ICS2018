b70ce1f225dfe66aec48d9d8d407e4feed423372
tracer-pa2018 <tracer@njuics.org>
2018-08-11 11:55:14 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  11:55:14 up  8:04, 24 users,  load average: 0.02, 0.02, 0.00 f260e0901b2c2c22d61a26ab451d714f8f98ae61

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -2,8 +2,9 @@
 
 
 static void instr_execute_1op() {
-	// printf("Calling LGDT.\n");
+	printf("lgdt...\n");
 	laddr_t mem_addr = opr_src.val;
+	printf("mem_addr = 0x%x\n", mem_addr);
 	cpu.gdtr.limit = laddr_read(mem_addr, 2) & 0xffff;
 	// printf("gdtr limit = 0x%x\n", cpu.gdtr.limit);
 	cpu.gdtr.base =  laddr_read(mem_addr + 2, 4) & 0xffffffff;
--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -3,8 +3,9 @@
 static void instr_execute_1op() {
 	printf("lidt..\n");
 	laddr_t mem_addr = opr_src.val;
-	cpu.idtr.limit = instr_fetch(mem_addr, 2) & 0xffff;
-	cpu.idtr.base = instr_fetch(mem_addr + 2, 4) & 0xffffffff;
+	printf("mem_addr = 0x%x\n", mem_addr);
+	cpu.idtr.limit = laddr_read(mem_addr, 2) & 0xffff;
+	cpu.idtr.base = laddr_read(mem_addr + 2, 4) & 0xffffffff;
 	if (data_size == 16)
 		cpu.idtr.base &= 0x00ffffff;
 	printf("limit = 0x%x, base = 0x%x\n", cpu.idtr.limit, cpu.idtr.base);
