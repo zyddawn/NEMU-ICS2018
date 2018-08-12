8b7c2dac65b197da21d7c8715feee15506f9ad4f
tracer-pa2018 <tracer@njuics.org>
2018-08-11 21:21:12 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:21:12 up 12:41, 27 users,  load average: 0.01, 0.04, 0.01 cea230176c4e996eff05d995896287e83ac1a55

--- nemu/src/memory/mmu/tlb.c
+++ nemu/src/memory/mmu/tlb.c
@@ -25,7 +25,7 @@ paddr_t page_walk(laddr_t laddr) {
 	PDE pde;
 	pde.val	= paddr_read(pdir_base + addr->pdir_idx * 4, 4);
 	if(!pde.present) {
-		Log("eip = %x, lnaddr = %x, pdir_base = %x, pde = %x", cpu.eip, lnaddr, pdir_base, pde.val);
+		Log("eip = %x, lnaddr = %x, pdir_base = %x, pde = %x", cpu.eip, laddr, pdir_base, pde.val);
 		assert(0);
 	}
 	assert(pde.present);
