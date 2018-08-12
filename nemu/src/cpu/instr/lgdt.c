3ad8be19f11b389767abe2b76fd11f2b360796e1
tracer-pa2018 <tracer@njuics.org>
2018-08-12 00:46:18 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  00:46:18 up 16:06, 31 users,  load average: 0.09, 0.08, 0.03 776f2d25fd8808321e7eea2d73df92de9a31b52

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -5,6 +5,7 @@ static void instr_execute_1op() {
 	printf("Calling LGDT.\n");
 	operand_read(&opr_src);
 	laddr_t mem_addr = opr_src.val;
+	printf("addr = 0x%x\n", mem_addr);
 	cpu.gdtr.limit = laddr_read(mem_addr, 2) & 0xffff;
 	printf("gdtr limit = 0x%x\n", cpu.gdtr.limit);
 	cpu.gdtr.base =  laddr_read(mem_addr + 2, 4) & 0xffffffff;
