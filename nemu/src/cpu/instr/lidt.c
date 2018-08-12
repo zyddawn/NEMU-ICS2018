d886e2d8437014bae4743550ba77cc287a340e9a
tracer-pa2018 <tracer@njuics.org>
2018-08-11 20:52:27 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:52:27 up 12:12, 26 users,  load average: 0.09, 0.04, 0.00 66ba78144b339c7b3845528ad6583e03882d06f0

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -9,7 +9,7 @@ static void instr_execute_1op() {
 	cpu.idtr.base = laddr_read(mem_addr + 2, 4) & 0xffffffff;
 	if (data_size == 16)
 		cpu.idtr.base &= 0x00ffffff;
-	printf("limit = 0x%x, base = 0x%x\n", cpu.idtr.limit, cpu.idtr.base);
+	printf("idtr: limit = 0x%x, base = 0x%x\n", cpu.idtr.limit, cpu.idtr.base);
 }
 
 make_instr_impl_1op(lidt, rm, v)
