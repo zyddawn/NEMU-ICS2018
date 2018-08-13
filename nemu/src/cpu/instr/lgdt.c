e9b2368b64c29a0bb658065d17393f1575728729
tracer-pa2018 <tracer@njuics.org>
2018-08-12 23:25:19 -0700

    >  nemu 141120159 daniel Linux ruby 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:25:19 up 16:20, 26 users,  load average: 0.03, 0.08, 0.04 92ee4da5935baa0b261027f5e32d62459a7a0d9

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -2,17 +2,15 @@
 
 
 static void instr_execute_1op() {
-	// printf("lgdt...\n");
+	printf("lgdt...\n");
 	laddr_t mem_addr = opr_src.val;
-	// printf("mem_addr = 0x%x\n", mem_addr);
+	printf("mem_addr = 0x%x\n", mem_addr);
 	cpu.gdtr.limit = laddr_read(mem_addr, 2) & 0xffff;
-	// printf("gdtr limit = 0x%x\n", cpu.gdtr.limit);
 	cpu.gdtr.base =  laddr_read(mem_addr + 2, 4) & 0xffffffff;
-	// printf("gdtr base = 0x%x\n", cpu.gdtr.base);
 	if (data_size == 16)
 		cpu.gdtr.base &= 0x00ffffff;
 		// not sure if it's correct
-	// printf("gdtr: limit = 0x%x, base = 0x%x\n", cpu.gdtr.limit, cpu.gdtr.base);	
+	printf("gdtr: limit = 0x%x, base = 0x%x\n", cpu.gdtr.limit, cpu.gdtr.base);	
 }
 
 
--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -2,16 +2,14 @@
 #include "memory/memory.h"
 
 static void instr_execute_1op() {
-	// printf("lidt..\n");
-	// operand_read(&opr_src);
-	// printf("src.val = 0x%x\n", opr_src.val);
+	printf("lidt..\n");
 	laddr_t mem_addr = opr_src.val;
-	// printf("mem_addr = 0x%x\n", mem_addr);
+	printf("mem_addr = 0x%x\n", mem_addr);
 	cpu.idtr.limit = laddr_read(mem_addr, 2) & 0xffff;
 	cpu.idtr.base = laddr_read(mem_addr + 2, 4) & 0xffffffff;
 	if (data_size == 16)
 		cpu.idtr.base &= 0x00ffffff;
-	// printf("idtr: limit = 0x%x, base = 0x%x\n", cpu.idtr.limit, cpu.idtr.base);
+	printf("idtr: limit = 0x%x, base = 0x%x\n", cpu.idtr.limit, cpu.idtr.base);
 }
 
 make_instr_impl_1op(lidt, rm, v)
